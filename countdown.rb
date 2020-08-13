3cc473017010473181b6e48e0e1c9b07d16a0649
April Adams <illusionlace@gmail.com>
2020-08-13 04:58:23 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,9 +7,9 @@ def countdown(number)
     number -= 1
     return "HAPPY NEW YEAR!"
 end
-end 
+end
 def countdown_with_sleep(number)
   number(5) = 1
   while number < 5
     number(5) -= 1
-end 
+end
