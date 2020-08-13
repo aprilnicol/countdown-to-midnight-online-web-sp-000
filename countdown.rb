8fca2aa753bcab1f4ce558edc7220f0f373b95f6
April Adams <illusionlace@gmail.com>
2020-08-13 05:01:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,14 +2,16 @@
 
 def countdown(number)
   number = 1
-  while number > 0
+  while number > 0 do 
     puts "#{number} SECOND(S)!"
     number -= 1
-    return "HAPPY NEW YEAR!"
 end
+"HAPPY NEW YEAR!"
 end
 def countdown_with_sleep(number)
-  number(5)
-  while number < 5
+  while number > 0 do 
+    puts "#{number} SECOND(S)!"
+    countdown 1
+    number -= 1
 end
 end 
